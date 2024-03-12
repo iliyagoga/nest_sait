4866526f7cf04f92434d2fe08914933d30c20e40
iliyagoga <100951861+iliyagoga@users.noreply.github.com>
2024-03-12 20:58:03 +0300

    update 5.2

--- src/components/client/Product.jsx
+++ src/components/client/Product.jsx
@@ -1,15 +1,27 @@
 import { observer } from "mobx-react-lite";
 import { apiMap } from "../../utilites/apiMap.ts";
 import def from '../../assets/imgs/def.png'
-
+import elipse from '../../assets/imgs/Ellipse 2.svg'
+import elipse_2 from '../../assets/imgs/Ellipse 1.svg'
+import Client from "../../stores/Client.ts";
 const Product = observer(({product})=>{
     console.log(product)
     return <div className="product">
         <div className="preview">
-          <img src={product.previews[0]!=undefined?(apiMap.host+":"+ apiMap.port +'/'+ product.previews[0].title): def} alt="" />
+            <div className="rating">
+                {product.ratingCount!=0?Array(Math.floor(product.rating/product.ratingCount)).map(v=>{return <img src={elipse}/>}): [1,2,3,4,5].map(v=>{return <img  className="rat" src={elipse_2}/>})}
+            </div>
+            {( (new Date()-new Date(432000))>new Date(product.createdAt))&&<div className="new">
+                <span>New</span>
+            </div>}
+            <img src={product.previews[0]!=undefined?(apiMap.host+":"+ apiMap.port +'/'+ product.previews[0].title): def} alt="" />
+        </div>
+        <div className="title">
+            <span>{product.productName}</span>
         </div>
         <div className="price">
-            <span>{product.price}</span>
+            <span className={product.sale_price?"old": ""}>{product.price} ₽</span>
+            {product.sale_price!=0&&<span className="sale">{product.sale_price} ₽</span>}
         </div>
     </div>
 })
